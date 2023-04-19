7c6851391aedd2cc522823aeaf4e5dde53c2d80a
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-19 12:47:20 -0400

    update css and button js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById("activity-container").textContent = data.activity;
-  });
+const BOTON = document.getElementById("btn");
+
+BOTON.addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("activity-container").textContent = data.activity;
+    });
+});
